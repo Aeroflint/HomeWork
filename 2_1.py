a1303bc3df699abb25904a52e411c07d73a7f858
Aeroflint <dimasbozhkov@gmail.com>
2023-09-14 01:04:42 +0300

    add result in version 1

---
+++ 2_1.py
@@ -0,0 +1,14 @@
+# TODO
+#  Пользователь вводит предложение, заменить все пробелы на "-" 2-мя способами
+
+# version 1.0
+a = input("Введите предложение!: ")
+sentence = a.split()
+result = "-".join(sentence)
+print(result)
+
+# Version 1.1
+a = input("Введите предложение!: ")
+print(a.replace(" ", "-"))
+
+
