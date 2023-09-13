b4467873efb8b4fe4aac6c293ea6e2ac6e121616
Aeroflint <dimasbozhkov@gmail.com>
2023-09-14 01:05:58 +0300

    add result in version 2

--- 2_1.py
+++ 2_1.py
@@ -1,14 +1,20 @@
 # TODO
 #  Пользователь вводит предложение, заменить все пробелы на "-" 2-мя способами
 
+
 # version 1.0
+
 a = input("Введите предложение!: ")
+
 sentence = a.split()
 result = "-".join(sentence)
 print(result)
 
-# Version 2.1
+
+# Version 2.0
+
 a = input("Введите предложение!: ")
+
 print(a.replace(" ", "-"))
 
 
