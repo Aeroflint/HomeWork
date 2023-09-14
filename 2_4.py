046b54c8dbf355a899304bdcdb757f04ee39080c
Aeroflint <dimasbozhkov@gmail.com>
2023-09-14 13:07:54 +0300

    finish task

--- 2_4.py
+++ 2_4.py
@@ -8,8 +8,9 @@ first_number = int(input("Введите первое число: "))
 second_number = int(input("Введите второе число: "))
 third_number = int(input("Введите третье число: "))
 
-result = (first_number > 0), (second_number > 0), (third_number > 0)
-
+result_true = (first_number > 0), (second_number > 0), (third_number > 0)
+result_false = (first_number >= 0), (second_number >= 0), (third_number >= 0)
 print((first_number > 0), (second_number > 0), (third_number > 0))
-print("Положительных значений - " + (str(result.count(True))))
-print("Отрицательных значений - " + (str(result.count(False))))
+print((first_number < 0), (second_number < 0), (third_number < 0))
+print("Положительных значений - " + (str(result_true.count(True))))
+print("Отрицательных значений - " + (str(result_false.count(False))))
