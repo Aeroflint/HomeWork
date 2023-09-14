8d92430fbbb2b376382d7f81c5d264dbf9e3abba
Aeroflint <dimasbozhkov@gmail.com>
2023-09-14 12:43:23 +0300

    finish task

--- 2_4.py
+++ 2_4.py
@@ -1,4 +1,15 @@
-first_number = float(input("Введите первое число: "))
-second_number = float(input("Введите второе число: "))
-third_number = float(input("Введите третье число: "))
 
+
+# TODO
+# Пользователь вводит 3 числа,
+# сказать сколько из них положительных и сколько отрицательных
+
+first_number = int(input("Введите первое число: "))
+second_number = int(input("Введите второе число: "))
+third_number = int(input("Введите третье число: "))
+
+result = (first_number > 0), (second_number > 0), (third_number > 0)
+
+print((first_number > 0), (second_number > 0), (third_number > 0))
+print("Положительных значений - " + (str(result.count(True))))
+print("Отрицательных значений - " + (str(result.count(False))))
