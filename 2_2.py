3b120708c661d8bdd05ba50e3b997baa98bb1236
Aeroflint <dimasbozhkov@gmail.com>
2023-09-14 11:16:20 +0300

    rename variables

--- 2_2.py
+++ 2_2.py
@@ -3,9 +3,18 @@
 # TODO
 # Пользователь вводит 3 числа, найти среднее арифмитическое с точность 3
 
+# first_number = float(input("Введите первое число!: "))
+# second_number = float(input("Введите второе число!: "))
+# third_number = float(input("Введите третье число!: "))
+#
+# list_number = (first_number + second_number+third_number)
+# result = sum(first_number+second_number+third_number) / len(list_number)
+# print(round(result, 3))
+
 first_number = float(input("Введите первое число!: "))
 second_number = float(input("Введите второе число!: "))
 third_number = float(input("Введите третье число!: "))
 
-result = (a+b+c / 3)
+list_number = [first_number, second_number, third_number]
+result = sum(list_number) / len(list_number)
 print(round(result, 3))
