69a31ec2b3b88004616685fd4685f088672b14c3
Aeroflint <dimasbozhkov@gmail.com>
2023-09-21 16:18:32 +0300

    finish task

---
+++ 3_2.py
@@ -0,0 +1,21 @@
+
+
+# TODO Без использования collections, написать программу, которая будет создавать словарь
+#  для подсчитывания количества вхождений каждой буквы в текст введенный с клавиатуры
+
+
+text = input("Введите текст!: ")
+counter = list(text)
+
+
+print(text_2)
+
+# Код анализа.
+#
+# text = 'hello world'
+# unique_letters = set(text)
+# analize = {}
+# for letter in unique_letters:
+#     analize[letter] = text.count(letter)
+#
+# print(counter)
