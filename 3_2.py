2c768b23fdcf1b870d3531bfc31e99db18e5b7e0
Aeroflint <dimasbozhkov@gmail.com>
2023-09-21 16:25:08 +0300

    finish task

--- 3_2.py
+++ 3_2.py
@@ -6,16 +6,3 @@
 
 text = input("Введите текст!: ")
 counter = list(text)
-
-
-print(text_2)
-
-# Код анализа.
-#
-# text = 'hello world'
-# unique_letters = set(text)
-# analize = {}
-# for letter in unique_letters:
-#     analize[letter] = text.count(letter)
-#
-# print(counter)
