a2279fb9d06f4d172bff49310b15e273b4baeabc
Aeroflint <dimasbozhkov@gmail.com>
2023-09-24 12:29:04 +0300

    finish task

--- 5_2.py
+++ 5_2.py
@@ -3,7 +3,7 @@
 # TODO Сделать калькулятор: у пользователя спрашивается число, потом действие и второе число
 
 number_1 =  int(input("Введите первое число!: "))
-symbol = input("Введите = - * / : ")
+symbol = input("Введите: '+', '-', '*', '/': ")
 number_2 = int(input("Введите второе число!: "))
 
 if symbol == "+":
@@ -13,11 +13,10 @@ elif symbol == "-":
 elif symbol == "*":
     answer = number_1 * number_2
 elif symbol == "/":
-    answer = number_1 / number_2
     if number_2 == 0:
         print("На 0 делить нельзя")
     else:
         answer = number_1 / number_2
 else:
-    print("Enter valid numbers")
-print(int(answer))
+    print("Введите верное значение")
+print(answer)
