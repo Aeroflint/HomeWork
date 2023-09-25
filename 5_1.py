6b6bb2ad721dadec824266b74c1cd8e7d446fb19
Aeroflint <dimasbozhkov@gmail.com>
2023-09-26 00:31:22 +0300

    finish task

--- 5_1.py
+++ 5_1.py
@@ -7,9 +7,12 @@ N = int(input("Введите число 1: "))
 M = int(input("Введите число 2: "))
 K = int(input("Введите число 3: "))
 
-for i in range(K, N, 2):
+for new in range(K,N,M):
     if N % M == 0 and N > K:
-        print(i)
+        print(new)
+    else:
+        print("Что-то не то!")
+
 
 
 
