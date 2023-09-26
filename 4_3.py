21763de983fa19dca691d3fd1de666d3881b16fc
Aeroflint <dimasbozhkov@gmail.com>
2023-09-26 10:20:00 +0300

    finish task

--- 4_3.py
+++ 4_3.py
@@ -4,12 +4,55 @@
 #  а значениями вложенный словарь с ключами "name" и "email", а значения
 #  для этих ключей будут браться с клавиатуры
 
-diction = {}
-name_result = input("Введите имя!: ")
-email_result = input("Введите email!: ")
+# d = {a: a ** 2 for a in range(7)}
 
-diction = {0: {
-    "name": name_result,
-    "email": email_result
+data = {}
+name = input("Введите имя: ")
+email = input("введите email: ")
+# n = (n for n in range(0, n)):
+# n = int(input("Введите число: "))
+n = int(input("Введите число!: "))
+# a = for i in range(0, n)
+a = (i for i in range (0, n))
+data = {n: {
+    'name': name,
+    'email': email
 }}
-print(diction)
+for i in range(0, n):
+print(data)
+
+
+
+
+
+
+
+
+# # for i in range(0, n):
+# print(dict.items(data))
+
+
+
+
+# data = {}
+# n = int(input("Введите число!: "))
+# name = input("Введите имя!: ")
+# email = input("Введите email!: ")
+#
+# data = {i for i in range(0, n)}
+#
+# # n = input("D")
+# # a = (int(n) + 5)
+# unique_data = dict()
+#
+# new_data = unique_data.pop(name, email)
+#
+# # data = {
+# #     0: {name: 'Alex', email: 'alex@gmail.com'},
+# #     1: {name: 'Petya', email: 'petya@gmail.com'},
+# #     2: {name: 'Masha', email: 'masha@gmail.com'}
+# # }
+# #
+# # print(data) * 3
+#
+# print(new_data(name, email))
